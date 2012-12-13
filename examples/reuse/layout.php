618d8d276b1917f5fe2b2ad35af57333b2897c47
hax <hax@heshijun.net>
2012-12-13 11:14:15 +0800

    update gitignore and remove generated php files

--- examples/reuse/layout.php
+++ examples/reuse/layout.php
@@ -1,1 +1,26 @@
-match failed
+<?php
+echo '<!doctype html>', "\n";
+
+ echo '<html';
+  echo '>';
+   echo '<head';
+    echo '>';
+     echo '<!-- start head -->';
+    	
+     //  #headBlock
+       echo '<!-- content of head -->';
+    	
+     echo '<!-- end head -->';
+    	
+   echo '</head>';
+   echo '<body';
+    echo '>';
+     echo '<!-- start body -->';
+    	
+     //  #bodyBlock
+       echo '<!-- content of body -->';
+    	
+     echo '<!-- end body -->';
+   echo '</body>';
+ echo '</html>';
+?>
--- examples/reuse/subtemplate.php
+++ examples/reuse/subtemplate.php
@@ -1,1 +1,25 @@
-match failed
+<?php
+echo '<!doctype html>', "\n";
+
+ echo '<html';
+  echo '>';
+   echo '<head';
+    echo '>';
+     echo '<!-- start head -->';
+    	
+    
+    	
+     echo '<!-- end head -->';
+    	
+   echo '</head>';
+   echo '<body';
+    echo '>';
+     echo '<!-- start body -->';
+    	
+    
+    	
+     echo '<!-- end body -->';
+   echo '</body>';
+ echo '</html>';
+	
+?>
