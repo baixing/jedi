68ba20e4d4685dabc523f6d699b4bdf244fb28c0
hax <hax@heshijun.net>
2012-12-13 10:59:38 +0800

    split transformers to individual file

--- src/transpiler.js
+++ src/transpiler.js
@@ -4,8 +4,9 @@ var ometajs = require('../lib/ometa-js')
 var fs = require('fs')
 var util = require('./util')
 
-var jedi = require('./jedi')
-var php = require('./transpiler.php5')
+var parser = require('./jedi')
+var transformer = require('./transformer')
+var transpiler = require('./transpiler.php5')
 
 var LineSep = /\r\n|\n\r?|\r|\u2028|\u2029/
 
@@ -14,15 +15,17 @@ function transpile(source, target) {
 
 function jedi2php(path) {
 	//var source = fs.readFileSync(path).toString()
-	var tree = jedi.Parser.match(path, 'load')
+	var tree = parser.Parser.match(path, 'load')
+	console.log('ast:')
 	util.dir(tree)
-	tree = jedi.InstructionsProcessor.match(tree, 'document')
+	tree = transformer.InstructionsProcessor.match(tree, 'document')
+	console.log('transformed:')
 	util.dir(tree)
-	tree = jedi.Sorter.match(tree, 'document')
-	tree = jedi.TemplateMatcher.match(tree, 'document')
+	tree = transformer.Sorter.match(tree, 'document')
+	tree = transformer.TemplateMatcher.match(tree, 'document')
 	//util.dir(tree)
 
-	var code = php.PHP5Transpiler.match(tree, 'document')
+	var code = transpiler.PHP5Transpiler.match(tree, 'document')
 	return code
 }
 
@@ -30,8 +33,9 @@ function compile(source, target) {
 	try {
 		fs.writeFileSync(target, jedi2php(source))
 	} catch(e) {
-		console.error(e)
-		fs.writeFileSync(target, e.stack || e.message || e)
+		var info = e.stack || e.message || e
+		console.error(info)
+		fs.writeFileSync(target, info)
 	}
 }
 
