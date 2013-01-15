f4bfbad1d8da8a0227f19a32ddaae0468b782e02
hax <hax@heshijun.net>
2013-01-16 05:21:25 +0800

    refactor public API and update cli

--- src/index.js
+++ src/index.js
@@ -1,1 +1,110 @@
-'use strict'var trans = require('./transpiler')var defaultPath = 'D:\\working\\hax\\haojing\\htdocs\\view\\listing.jedi'if (process.argv[2] === '-w') {	trans.watch(require.resolve(process.argv[3] || defaultPath))} else {	console.log(trans.jedi2php('./examples/hello.jedi'))}
+/* Jedi public API */
+
+'use strict'
+
+require('../lib/ometa-js')
+
+var Parser = require('./parser').Parser
+var transformer = require('./transformer')
+var transpiler = {
+	php5: require('./transpiler.php5').PHP5Transpiler,
+	es5: require('./transpiler.es5').ES5Transpiler
+}
+
+var fs = require('fs'), path = require('path')
+var util = require('./util')
+
+var Class = require('mmclass').Class
+var FileCache = require('./Cache').FileCache
+
+var ParserCache = Class.extend(FileCache)({
+	content: function (file) {
+		return Parser.match(file, 'load')
+	}
+})
+
+var cache = new ParserCache()
+
+function parseFile(filename) {
+	return cache.get(path.resolve(filename))
+}
+
+function transform(tree, debug) {
+	var tree1 = tree
+	if (debug[0]) util.dir(tree1)
+	var tree2 = transformer.InstructionsProcessor.match(tree1, 'document')
+	if (debug[1]) util.dir(tree2)
+	var tree3 = transformer.TemplateMatcher.match(tree2, 'document')
+	if (debug[2]) util.dir(tree3)
+	var tree4 = transformer.Sorter.match(tree3, 'document')
+	if (debug[3]) util.dir(tree4)
+	return tree4
+}
+
+function compile(ast, target) {
+	switch (target) {
+		case 'php5': case 'php':
+			return transpiler.php5.match(ast, 'document')
+		case 'es5': case 'ecmascript':
+		case 'js': case 'javascript':
+			return transpiler.es5.match(ast, 'document')
+		default:
+			throw Error('Unknown target language: ' + target)
+	}
+}
+
+function transpile(source, dest, lang, debug) {
+	try {
+		var tree = transform(parseFile(source), debug)
+		fs.writeFileSync(dest, compile(tree, lang))
+	} catch(e) {
+		var info = e.stack || e.message || e
+		console.error('Error: ', String(info))
+		fs.writeFileSync(dest, info)
+	}
+}
+
+function watch(source, dest, lang, debug) {
+	//TODO: watch dependencies
+	transpile(source, dest, lang, debug)
+	fs.watch(source, function(evt, filename) {
+		transpile(source, dest, lang, debug)
+	})
+}
+
+function service(options) {
+	//var watched = []
+
+	http.createServer(function (req, res) {
+		switch (req.method) {
+			case 'GET':
+				var f = path.join(options.base, url.parse(req.url).path)
+				//if (watched.indexOf(path) >= 0)
+
+				fs.exists(f, function(exists){
+					if (!exists) {
+						res.writeHead(404)
+						res.end('file not exist\n')
+					} else {
+						transpile(f, f.replace(/(.jedi)$/, '.php'))
+						res.writeHead(200)
+						res.end('compiled ok\n')
+					}
+				})
+
+				//transpiler.watch(loc.pathname)
+				break
+			default:
+				res.writeHead(405)
+				res.end()
+		}
+	}).listen(options.port)
+
+}
+
+//exports.parseFile = parseFile
+//exports.transform = transform
+//exports.compile = compile
+exports.transpile = transpile
+exports.watch = watch
+exports.service = service
