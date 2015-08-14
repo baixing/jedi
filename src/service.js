ed6e399d9db4a162f21febd0c138457fae8897a3
HE Shi-Jun <hax@heshijun.net>
2015-08-14 12:48:37 +0800

    refactor to es6

--- src/service.js
+++ src/service.js
@@ -1,62 +1,60 @@
-var http = require('http'), url = require('url')
+import {createServer as httpServer} from 'http'
+import {parse as parseURL} from 'url'
+import {join as joinPath} from 'path'
+import {exists as existsFile, stat} from 'fs'
 import {transpile} from '.'
 
-export function service(options) {
+export function service({base, lang, port}) {
 	//var watched = []
 
-	http.createServer(function (req, res) {
+	httpServer((req, res) => {
 
-		switch (req.method) {
-			case 'GET':
-				//console.log(req.url)
-				var p = url.parse(req.url).path
-				//console.log(options.base, p)
-				var f = path.join(options.base, p)
-				f = f.replace(/^\\\\([A-Z])\|/, '$1:')
-				//if (watched.indexOf(path) >= 0)
+		if (req.method === 'GET') {
+			let f = joinPath(base, parseURL(req.url).pathname)
+			f = f.replace(/^\\\\([A-Z])\|/, '$1:')
+			//if (watched.indexOf(path) >= 0)
+			const send = (status, message) => {
+				res.writeHead(status)
+				res.end(message + ': ' + f + '\n')
+				if (status >= 400) {
+					console.error(message + ': ' + f)
+					console.error()
+				} else {
+					console.info(message + ': ' + f)
+					console.info()
+				}
+			}
 
-				fs.exists(f, function(exists){
-					if (!exists) {
-						send(404, 'file not exist')
-					} else {
-						fs.stat(f, function(err, stats){
-							if (err) throw err // should never happen
-							if (stats.isFile()) {
-								var t0 = Date.now()
-								options.lang.forEach(function(lang){
-									transpile(f, f.replace(/\.jedi$/, '.' + lang), lang)
-								})
-								var t1 = Date.now()
-								send(200, 'transpiled in ' + (t1 - t0) + 'ms')
-							} else {
-								send(404, 'path is not a file')
-							}
-						})
-					}
-
-					function send(status, message){
-						res.writeHead(status)
-						res.end(message + ': ' + f + '\n')
-						if (status >= 400) {
-							console.error(message + ': ' + f)
-							console.error()
+			existsFile(f, exists => {
+				if (!exists) {
+					send(404, 'file not exist')
+				} else {
+					stat(f, (err, stats) => {
+						if (err) throw err // should never happen
+						if (stats.isFile()) {
+							const t0 = Date.now()
+							lang.forEach(function(lang){
+								transpile(f, f.replace(/\.jedi$/, '.' + lang), lang)
+							})
+							const t1 = Date.now()
+							send(200, 'transpiled in ' + (t1 - t0) + 'ms')
 						} else {
-							console.info(message + ': ' + f)
-							console.info()
+							send(404, 'path is not a file')
 						}
-					}
-				})
+					})
+				}
 
-				//transpiler.watch(loc.pathname)
-				break
-			default:
-				res.writeHead(405)
-				res.end()
+			})
+
+			//transpiler.watch(loc.pathname)
+		} else {
+			res.writeHead(405)
+			res.end()
 		}
 
-	}).listen(options.port)
+	}).listen(port)
 
-	process.on('uncaughtException', function(err){
+	process.on('uncaughtException', err => {
 		console.error(new Date().toISOString(), 'uncaught exception:', err)
 		console.trace(err)
 	})
