9b2c7ee35da8d006cfe3f19551b27c3ae367d7f8
HE Shi-Jun <hax@heshijun.net>
2015-08-18 17:06:58 +0800

    fix load order of source-map-support

--- test/index.js
+++ test/index.js
@@ -1,2 +1,5 @@
+// must load coffee-script before source-map-support due to
+// https://github.com/evanw/node-source-map-support/issues/34
+require('coffee-script/register')
 require('../register')
 require('./suites')
