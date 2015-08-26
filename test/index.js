32871e770e2c99a7daab0f5f21ff4e0ab206c44d
HE Shi-Jun <hax@heshijun.net>
2015-08-26 18:15:03 +0800

    update test entry

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 // must load coffee-script before source-map-support due to
 // https://github.com/evanw/node-source-map-support/issues/34
 require('coffee-script/register')
-require('../register')
+require('source-map-support').install()
+require('babel/register')
+require('../lib/ometa-js')
 require('./suites')
