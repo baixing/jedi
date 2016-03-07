73b9f10f6aeb001ede571039b66e65958e5d7bf9
HE Shi-Jun <hax@heshijun.net>
2016-03-08 02:31:50 +0800

    fix var name

--- src/util/adapter.js
+++ src/util/adapter.js
@@ -2,17 +2,17 @@ import {skip, hasChildNodes} from './node'
 
 export const tuple2record = (t) => {
 	if (skip(t)) return {nodeType: 'skip', data: t}
-	const [nodeType, position, ...data] = t
-	const pos = [...position] // copy to avoid being cached
+	const [nodeType, pos, ...data] = t
+	const position = [...pos] // copy to avoid being cached
 	if (nodeType === 'element') {
 		const [[tagName, classList, id], binding, childNodes] = data
-		return ({nodeType, pos, nodeName: tagName, tagName, classList, id, binding, childNodes})
+		return ({nodeType, position, nodeName: tagName, tagName, classList, id, binding, childNodes})
 	}
 	if (hasChildNodes(nodeType)) {
 		const [nodeName, nodeValue, childNodes] = data
-		return {nodeType, pos, nodeName, nodeValue, childNodes}
+		return {nodeType, position, nodeName, nodeValue, childNodes}
 	}
-	return {nodeType, pos, data}
+	return {nodeType, position, data}
 }
 
 export const record2tuple = ({nodeType, position, ...data}) => {
