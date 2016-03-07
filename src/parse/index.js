34c1518c8767b096621e42de30e582dda2b3ce13
HE Shi-Jun <hax@heshijun.net>
2016-03-07 18:03:24 +0800

    fix cache problem by clone nodes

--- src/parse/index.js
+++ src/parse/index.js
@@ -14,4 +14,6 @@ const splitLines = s => s.split(/\r?\n|[\u2028\u2029]/)
 import memoize from '../util/memoize'
 import {Parser} from './parser'
 const match = Parser.match::memoize(JSON.stringify)
-export const parseLines = lines => Parser::match(lines, 'block')
+
+import clone from '../util/clone'
+export const parseLines = lines => clone(Parser::match(lines, 'block'))
