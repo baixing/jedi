43d793e6462043c9ea717b91defd791e6c569d98
HE Shi-Jun <hax@heshijun.net>
2016-03-07 18:24:32 +0800

    fix cache problem

--- src/util/adapter.js
+++ src/util/adapter.js
@@ -3,15 +3,16 @@ import {skip, hasChildNodes} from './node'
 export const tuple2record = (t) => {
 	if (skip(t)) return {nodeType: 'skip', data: t}
 	const [nodeType, position, ...data] = t
+	const pos = [...position] // copy to avoid being cached
 	if (nodeType === 'element') {
 		const [[tagName, classList, id], binding, childNodes] = data
-		return ({nodeType, position, nodeName: tagName, tagName, classList, id, binding, childNodes})
+		return ({nodeType, pos, nodeName: tagName, tagName, classList, id, binding, childNodes})
 	}
 	if (hasChildNodes(nodeType)) {
 		const [nodeName, nodeValue, childNodes] = data
-		return {nodeType, position, nodeName, nodeValue, childNodes}
+		return {nodeType, pos, nodeName, nodeValue, childNodes}
 	}
-	return {nodeType, position, data}
+	return {nodeType, pos, data}
 }
 
 export const record2tuple = ({nodeType, position, ...data}) => {
