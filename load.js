d552615a98154d8ec11e3c6f39143fec2172492b
HE Shi-Jun <hax@heshijun.net>
2016-06-21 16:11:34 +0800

    upgrade to babel 6.x

--- load.js
+++ load.js
@@ -2,13 +2,13 @@ module.exports = load
 
 function load(mod) {
 	if (/[/\\]node_modules[/\\]jedi$/.test(__dirname)) {
-		require('babel/polyfill')
+		require('babel-polyfill')
 		require('./lib/ometa-js')
 		return require('./lib/' + mod)
 	} else { // dev env
 		console.info('jedi development version')
 		require('source-map-support').install()
-		require('babel/register')
+		require('babel-register')
 		require('./lib/ometa-js')
 		return require('./src/' + mod)
 	}
--- test/index.js
+++ test/index.js
@@ -2,6 +2,6 @@
 // https://github.com/evanw/node-source-map-support/issues/34
 require('coffee-script/register')
 require('source-map-support').install()
-require('babel/register')
+require('babel-register')
 require('../lib/ometa-js')
 require('./suites')
