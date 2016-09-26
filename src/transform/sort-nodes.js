9b905c5461e9e5377d5e1ea10edf6a0fa1c80870
HE Shi-Jun <hax@heshijun.net>
2016-09-26 19:34:33 +0800

    refactor traverse to enter/leave

--- src/transform/sort-nodes.js
+++ src/transform/sort-nodes.js
@@ -2,59 +2,60 @@ import traverse from '../util/traverse'
 import {record2tuple} from '../util/adapter'
 
 export default function sortNodes(tree) {
-	return tree::traverse(node => {
-		if (node.childNodes) {
-			const attributes = [], macros = [], normalNodes = []
-			let skips = []
-			const flush = to => {
-				if (skips.length > 0) {
-					to.push(...skips)
-					skips = []
+	return tree::traverse({
+		leave(node) {
+			if (node.childNodes) {
+				const attributes = [], macros = [], normalNodes = []
+				let skips = []
+				const flush = to => {
+					if (skips.length > 0) {
+						to.push(...skips)
+						skips = []
+					}
 				}
-			}
-			node.childNodes::traverse(node => {
-				switch (node.nodeType) {
-					case 'skip':
-					case 'suppress':
-					case 'inject':
-						skips.push(node)
-						break
-					case 'attribute':
-						flush(attributes)
-						attributes.push(node)
-						break
-					case 'macro':
-						flush(macros)
-						macros.push(node)
-						break
-					case 'instruction':
-						if (node.nodeName === 'external') {
+				node.childNodes::traverse(node => {
+					switch (node.nodeType) {
+						case 'skip':
+						case 'suppress':
+						case 'inject':
+							skips.push(node)
+							break
+						case 'attribute':
+							flush(attributes)
+							attributes.push(node)
+							break
+						case 'macro':
 							flush(macros)
 							macros.push(node)
 							break
-						}
-					case 'fragment': // see https://github.com/baixing/jedi/issues/65
-						if (node.nodeName.slice(-1) === '#') {
-							skips.push(node)
+						case 'instruction':
+							if (node.nodeName === 'external' || node.nodeName === 'use') {
+								flush(macros)
+								macros.push(node)
+								break
+							}
+						case 'fragment': // see https://github.com/baixing/jedi/issues/65
+							if (node.nodeName.slice(-1) === '#') {
+								skips.push(node)
+								break
+							}
+						default:
+							flush(normalNodes)
+							normalNodes.push(node)
 							break
-						}
-					default:
-						flush(normalNodes)
-						normalNodes.push(node)
-						break
+					}
+					return false
+				}, true)
+				flush(normalNodes)
+				const sorted = macros.concat(normalNodes)
+				if (node.nodeType === 'element' || node.nodeType === 'macro') {
+					sorted.unshift(...attributes, {nodeType: 'skip', data: ['closeStartTag']})
+				} else if (attributes.length > 0) {
+					const e = new Error('OnlyElementAllowAttribute')
+					e.position = attributes[0].position
+					throw e
 				}
-				return false
-			}, undefined, true)
-			flush(normalNodes)
-			const sorted = macros.concat(normalNodes)
-			if (node.nodeType === 'element' || node.nodeType === 'macro') {
-				sorted.unshift(...attributes, {nodeType: 'skip', data: ['closeStartTag']})
-			} else if (attributes.length > 0) {
-				const e = new Error('OnlyElementAllowAttribute')
-				e.position = attributes[0].position
-				throw e
 			}
-			node.childNodes = sorted.map(record2tuple)
 		}
-	}, 'post')
+	})
 }
--- src/util/query.js
+++ src/util/query.js
@@ -1,7 +1,7 @@
 import {record2tuple} from './adapter'
 import traverse from './traverse'
 
-export function query(f, order) {
+export function query(f) {
 	let match1
 	this::traverse(node => {
 		if (match1) return false
@@ -9,33 +9,14 @@ export function query(f, order) {
 			match1 = node
 			return false
 		}
-	}, order)
+	})
 	return match1 ? record2tuple(match1) : match1
 }
 
-export function queryAll(f, order) {
+export function queryAll(f) {
 	const matches = []
 	this::traverse(node => {
 		if (f(node)) matches.push(node)
-	}, order)
+	})
 	return matches
 }
-
-// export function match(pattern) {
-// 	if (typeof pattern === 'function' && pattern.prototype) return this instanceof pattern
-// 	if (pattern && typeof pattern.test === 'function') return pattern.test(this)
-// 	switch (typeof this) {
-// 		case 'undefined':
-// 		case 'boolean':
-// 		case 'number':
-// 		case 'string':
-// 		case 'symbol':
-// 			return this === pattern
-// 		case 'object':
-// 			if (this === null) return pattern === null
-// 			if (Array.isArray(pattern)) return pattern.every((p, i) => this[i]::match(p))
-// 			if (typeof pattern === 'object') return Object.keys(pattern).every(key => this[key]::match(pattern[key]))
-// 			return false
-// 		default: throw new Error('should not be function')
-// 	}
-// }
